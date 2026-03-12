0b72e3968287ad354888bdf5618595f99f469e65
Stephen <stephengreer97@gmail.com>
2026-03-12 10:35:25 -0500

    feat: add /logo route

---
+++ app/logo/page.tsx
@@ -0,0 +1,7 @@
+export default function LogoPage() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', background: '#fff' }}>
+      <img src="/icon1024.png" alt="Mealio" style={{ width: 1024, height: 1024, maxWidth: '100vw', maxHeight: '100vh', objectFit: 'contain' }} />
+    </div>
+  );
+}
